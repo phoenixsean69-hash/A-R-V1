5a0f67b48a016a3ddf4c03afe06c430af1dabc6c
phoenix69 <phoenixsean69@gmail.com>
2026-07-28 19:16:39 +0200

    darker theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/typographyScale.css";
 import "maplibre-gl/dist/maplibre-gl.css";
+import "./styles/darkerTheme.css";
 
 import App from "./App.tsx";
 import { migratePreciseSceneCollisionConfirmation } from "./utils/migratePreciseSceneCollisionConfirmation";
